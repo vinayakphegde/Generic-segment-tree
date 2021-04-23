c8f7981dd6f6edc1d1d989c058d9dd5eb15bb2b5
RahulRavishankar <rahul.k.ravishankar@gmail.com>
2021-04-23 23:13:53 +0530

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -6,20 +6,30 @@ using namespace std;
 
 int main()
 {
-	int n;
-	n=8;
- 	list<int> v{1,2,3,4,5,6,7,8};
-	int sum=0;
-	segment_tree<int,operation::min<int>, list<int>::iterator> tree1(v.begin(),v.end());
-	tree1.display();
-	cout<< tree1.query(0,5) << "\n";
-	tree1.display();
-	tree1.update(1,4);
-	tree1.display();
-
 	vector<int> a = {2,3,5,6,4,8,2,1,4,6};
-	segment_tree<int, operation::sum<int>,vector<int>::iterator> tree(a.begin()+2,a.begin()+a.size()-2);
-	tree.display();
-	cout<<tree.query(1,3)<<"\n";
+	// segment_tree<int, operation::sum<int> > tree(a.begin()+2,a.begin()+a.size()-2);
+	// tree.display();
+	// cout<<tree.query(1,3)<<"\n";
+
+	cout<<"\n"<<"SUM: "<<"\n";
+	segment_tree<int, operation::sum<int> > tree(a.begin(),a.end());
+	for(int i=1;i<=a.size();++i)
+	{
+		cout<<tree.query(0,i)<<"\n";
+	}
+
+	cout<<"\n"<<"MIN: "<<"\n";
+	segment_tree<int, operation::min<int> > tree2(a.begin(),a.end());
+	for(int i=1;i<=a.size();++i)
+	{
+		cout<<*tree2.query(0,i)<<"\n";
+	}
+
+	cout<<"\n"<<"MAX: "<<"\n";
+	segment_tree<int, operation::max<int> > tree3(a.begin(),a.end());
+	for(int i=1;i<=a.size();++i)
+	{
+		cout<<*tree3.query(0,i)<<"\n";
+	}
 	return 0;	
 }
